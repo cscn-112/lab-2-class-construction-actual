99c6bdc1b776e51e7e1d051018a390514e318ef0
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-09-30 18:20:34 -0400

    feat(C++): add main

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+    StudentType liberty_student;
+
+    liberty_student.set_first_name("Benjamin");
+    liberty_student.last_name = "Laird";
+
+    liberty_student.display_first_name();
+    std::cout << " ";
+    std::cout << liberty_student.last_name;
 
     return 0;
 }
