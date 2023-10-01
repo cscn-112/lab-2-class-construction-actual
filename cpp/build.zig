a755fc9b23d55c846ea276b2ac18760e7ab8586a
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-09-30 20:01:43 -0400

    hopefully

--- cpp/build.zig
+++ cpp/build.zig
@@ -27,7 +27,7 @@ pub fn build(b: *std.Build) void {
 
     exe.linkLibCpp();
 
-    exe.linkSystemLibrary("Student.cpp");
+    // exe.linkSystemLibrary("Student.cpp");
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
