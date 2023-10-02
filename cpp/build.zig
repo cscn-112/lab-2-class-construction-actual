1b5122bad81a753aee62da582b96076bd65a4878
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2023-10-01 21:39:20 -0400

    feat(C++): overhaul zig build system, ci

--- cpp/build.zig
+++ cpp/build.zig
@@ -1,59 +1,59 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) !void {
+    var sources = std.ArrayList([]const u8).init(b.allocator);
+
+    // Search for all C/C++ files in `src/lib` and add them
+    {
+        const base_path: []const u8 = "src/lib";
+        const allowed_exts = [_][]const u8{ ".c", ".cpp", ".cxx", ".c++", ".cc" };
+
+        var dir = try std.fs.cwd().openIterableDir(base_path, .{});
+
+        var walker = try dir.walk(b.allocator);
+        defer walker.deinit();
+
+        while (try walker.next()) |entry| {
+            const ext = std.fs.path.extension(entry.basename);
+            const include_file = for (allowed_exts) |e| {
+                if (std.mem.eql(u8, ext, e))
+                    break true;
+            } else false;
+
+            if (include_file) {
+                var large_buffer: [100]u8 = undefined;
+                const large_buffer_slice = large_buffer[0..];
+
+                // we have to clone the path as walker.next() or walker.deinit() will override/kill it
+                const full_path = try std.fmt.bufPrint(large_buffer_slice, "{s}/{s}", .{ base_path, b.dupe(entry.path) });
+
+                try sources.append(full_path);
+            }
+        }
+    }
+
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "lab-2-class-construction",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/**.cpp" },
-        // .main_pkg_path = .{ .path = "src" },
+        .root_source_file = .{ .path = "src/main.cpp" },
         .target = target,
         .optimize = optimize,
     });
-
+    exe.addCSourceFiles(sources.items, &[_][]const u8{});
     exe.linkLibCpp();
-
-    // exe.linkSystemLibrary("Student.cpp");
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Student.h"
+#include "lib/Student.h"
 
 int main()
 {
